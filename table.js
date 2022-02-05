0fc3edac8e9889ab74be870cd318e1663f4939ed
seerrazuriz <seerrazuriz@uc.cl>
2022-02-05 08:03:23 -0300

    subir repo

---
+++ table.js
@@ -0,0 +1,516 @@
+const data = [
+    {
+      "rut": "13155811-2",
+      "nombre": "adelardo linares pina",
+      "correo": "adelardo.linares.pina@gmail.com",
+      "telefono": 933662161,
+      "rol": "Encargado",
+      "empresa": "24566099-5 Industrial & Commercial Bank of China",
+      "fecha creacion": "15/06/2021"
+    },
+    {
+      "rut": "9557194-8",
+      "nombre": "agueda boix luz",
+      "correo": "agueda.boix.luz@gmail.com",
+      "telefono": 918415720,
+      "rol": "Operador",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "29/06/2021"
+    },
+    {
+      "rut": "11099974-7",
+      "nombre": "aitor valentin angel",
+      "correo": "aitor.valentin.angel@hotmail.com",
+      "telefono": 910402371,
+      "rol": "Ejecutivo",
+      "empresa": "23690053-3 Berkshire Hathaway",
+      "fecha creacion": "1/10/2021"
+    },
+    {
+      "rut": "22838951-k",
+      "nombre": "amada saenz barranco",
+      "correo": "amada.saenz.barranco@gmail.com",
+      "telefono": 956262188,
+      "rol": "Encargado",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "14/04/2021"
+    },
+    {
+      "rut": "7094964-4",
+      "nombre": "amado lucena solis",
+      "correo": "amado.lucena.solis@hotmail.com",
+      "telefono": 945384440,
+      "rol": "Ejecutivo",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "17/03/2021"
+    },
+    {
+      "rut": "14888047-6",
+      "nombre": "america brigida barragan",
+      "correo": "america.brigida.barragan@gmail.com",
+      "telefono": 918835102,
+      "rol": "Gestor",
+      "empresa": "12043514-0 China Construction Bank",
+      "fecha creacion": "25/04/2021"
+    },
+    {
+      "rut": "11704098-4",
+      "nombre": "aranzazu lucena vizcaino",
+      "correo": "aranzazu.lucena.vizcaino@hotmail.com",
+      "telefono": 904350077,
+      "rol": "Gestor",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "13/02/2021"
+    },
+    {
+      "rut": "12342781-5",
+      "nombre": "arcelia martinez rossello",
+      "correo": "arcelia.martinez.rossello@hotmail.com",
+      "telefono": 959167361,
+      "rol": "Ejecutivo",
+      "empresa": "22724464-k Agricultural Bank of China",
+      "fecha creacion": "20/05/2021"
+    },
+    {
+      "rut": "8783899-4",
+      "nombre": "aristides fortuny pozo",
+      "correo": "aristides.fortuny.pozo@hotmail.com",
+      "telefono": 929359280,
+      "rol": "Encargado",
+      "empresa": "11578774-8 Alphabet",
+      "fecha creacion": "28/06/2021"
+    },
+    {
+      "rut": "15586230-0",
+      "nombre": "aurea riquelme gomis",
+      "correo": "aurea.riquelme.gomis@hotmail.com",
+      "telefono": 971854899,
+      "rol": "Ejecutivo",
+      "empresa": "22724464-k Agricultural Bank of China",
+      "fecha creacion": "26/05/2021"
+    },
+    {
+      "rut": "20854705-4",
+      "nombre": "casandra palomares-folch andrade",
+      "correo": "casandra.palomares-folch.andrade@gmail.com",
+      "telefono": 912269611,
+      "rol": "Operador",
+      "empresa": "11578774-8 Alphabet",
+      "fecha creacion": "2/01/2021"
+    },
+    {
+      "rut": "23827119-3",
+      "nombre": "chus de palma",
+      "correo": "chus.de.palma@gmail.com",
+      "telefono": 973860118,
+      "rol": "Operador",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "24/03/2021"
+    },
+    {
+      "rut": "20404014-1",
+      "nombre": "cleto agusti roman",
+      "correo": "cleto.agusti.roman@yahoo.es",
+      "telefono": 961317886,
+      "rol": "Gestor",
+      "empresa": "11578774-8 Alphabet",
+      "fecha creacion": "19/01/2021"
+    },
+    {
+      "rut": "6843510-2",
+      "nombre": "cosme feliu clemente",
+      "correo": "cosme.feliu.clemente@hotmail.com",
+      "telefono": 935619309,
+      "rol": "Ejecutivo",
+      "empresa": "12043514-0 China Construction Bank",
+      "fecha creacion": "21/05/2021"
+    },
+    {
+      "rut": "5502325-5",
+      "nombre": "daniela torrents alarcon",
+      "correo": "daniela.torrents.alarcon@gmail.com",
+      "telefono": 966034177,
+      "rol": "Operador",
+      "empresa": "12043514-0 China Construction Bank",
+      "fecha creacion": "1/09/2021"
+    },
+    {
+      "rut": "17264567-4",
+      "nombre": "emiliana corominas grande",
+      "correo": "emiliana.corominas.grande@hotmail.com",
+      "telefono": 930812962,
+      "rol": "Ejecutivo",
+      "empresa": "22724464-k Agricultural Bank of China",
+      "fecha creacion": "17/04/2021"
+    },
+    {
+      "rut": "23288618-8",
+      "nombre": "faustino martin lumbreras",
+      "correo": "faustino.martin.lumbreras@yahoo.es",
+      "telefono": 946546715,
+      "rol": "Gestor",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "23/04/2021"
+    },
+    {
+      "rut": "24814475-0",
+      "nombre": "federico cuenca-hernandez palma",
+      "correo": "federico.cuenca-hernandez.palma@yahoo.es",
+      "telefono": 971435180,
+      "rol": "Ejecutivo",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "26/10/2021"
+    },
+    {
+      "rut": "22316059-k",
+      "nombre": "fito javi bayon",
+      "correo": "fito.javi.bayon@gmail.com",
+      "telefono": 997007137,
+      "rol": "Gestor",
+      "empresa": "12043514-0 China Construction Bank",
+      "fecha creacion": "7/07/2021"
+    },
+    {
+      "rut": "24168165-3",
+      "nombre": "flavia mateo chacon",
+      "correo": "flavia.mateo.chacon@gmail.com",
+      "telefono": 961293529,
+      "rol": "Encargado",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "12/07/2021"
+    },
+    {
+      "rut": "19925685-8",
+      "nombre": "francisco javier ortuño",
+      "correo": "francisco.javier.ortuño@gmail.com",
+      "telefono": 999208609,
+      "rol": "Ejecutivo",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "1/02/2021"
+    },
+    {
+      "rut": "7649360-k",
+      "nombre": "francisco pazos-peña vidal",
+      "correo": "francisco.pazos-peña.vidal@yahoo.es",
+      "telefono": 935318400,
+      "rol": "Operador",
+      "empresa": "22724464-k Agricultural Bank of China",
+      "fecha creacion": "18/01/2021"
+    },
+    {
+      "rut": "11937133-3",
+      "nombre": "fulgencio peinado cisneros",
+      "correo": "fulgencio.peinado.cisneros@gmail.com",
+      "telefono": 906224400,
+      "rol": "Gestor",
+      "empresa": "23690053-3 Berkshire Hathaway",
+      "fecha creacion": "14/02/2021"
+    },
+    {
+      "rut": "14314886-6",
+      "nombre": "hilario esteban miro",
+      "correo": "hilario.esteban.miro@yahoo.es",
+      "telefono": 973667023,
+      "rol": "Gestor",
+      "empresa": "22724464-k Agricultural Bank of China",
+      "fecha creacion": "19/05/2021"
+    },
+    {
+      "rut": "22007021-2",
+      "nombre": "iban villanueva gaya",
+      "correo": "iban.villanueva.gaya@gmail.com",
+      "telefono": 908309835,
+      "rol": "Encargado",
+      "empresa": "14305598-1 Microsoft",
+      "fecha creacion": "14/09/2021"
+    },
+    {
+      "rut": "7892094-7",
+      "nombre": "isidoro torrijos ayala",
+      "correo": "isidoro.torrijos.ayala@gmail.com",
+      "telefono": 923990021,
+      "rol": "Operador",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "28/09/2021"
+    },
+    {
+      "rut": "7141511-2",
+      "nombre": "jacinto blasco calatayud",
+      "correo": "jacinto.blasco.calatayud@gmail.com",
+      "telefono": 914047806,
+      "rol": "Operador",
+      "empresa": "14305598-1 Microsoft",
+      "fecha creacion": "23/10/2021"
+    },
+    {
+      "rut": "21899480-6",
+      "nombre": "jafet del izaguirre",
+      "correo": "jafet.del.izaguirre@hotmail.com",
+      "telefono": 991664125,
+      "rol": "Operador",
+      "empresa": "14305598-1 Microsoft",
+      "fecha creacion": "7/06/2021"
+    },
+    {
+      "rut": "21313021-8",
+      "nombre": "jesusa cortes-bejarano solis",
+      "correo": "jesusa.cortes-bejarano.solis@yahoo.es",
+      "telefono": 919796172,
+      "rol": "Ejecutivo",
+      "empresa": "12039291-3 Saudi Aramco",
+      "fecha creacion": "6/07/2021"
+    },
+    {
+      "rut": "22537935-1",
+      "nombre": "juan antonio cortes-vergara",
+      "correo": "juan.antonio.cortes-vergara@hotmail.com",
+      "telefono": 994018768,
+      "rol": "Operador",
+      "empresa": "10882185-k SoftBank Group",
+      "fecha creacion": "20/02/2021"
+    },
+    {
+      "rut": "23509369-3",
+      "nombre": "lola camara rossello",
+      "correo": "lola.camara.rossello@gmail.com",
+      "telefono": 937507655,
+      "rol": "Gestor",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "28/06/2021"
+    },
+    {
+      "rut": "22615548-1",
+      "nombre": "marco alfonso rius",
+      "correo": "marco.alfonso.rius@gmail.com",
+      "telefono": 928582123,
+      "rol": "Gestor",
+      "empresa": "24566099-5 Industrial & Commercial Bank of China",
+      "fecha creacion": "14/01/2021"
+    },
+    {
+      "rut": "10425328-8",
+      "nombre": "maria del camacho",
+      "correo": "maria.del.camacho@hotmail.com",
+      "telefono": 981039662,
+      "rol": "Encargado",
+      "empresa": "23690053-3 Berkshire Hathaway",
+      "fecha creacion": "6/09/2021"
+    },
+    {
+      "rut": "7297115-9",
+      "nombre": "maria pilar alfaro",
+      "correo": "maria.pilar.alfaro@gmail.com",
+      "telefono": 999871654,
+      "rol": "Ejecutivo",
+      "empresa": "12039291-3 Saudi Aramco",
+      "fecha creacion": "14/09/2021"
+    },
+    {
+      "rut": "10299976-2",
+      "nombre": "maricruz escolano vidal",
+      "correo": "maricruz.escolano.vidal@hotmail.com",
+      "telefono": 973042743,
+      "rol": "Gestor",
+      "empresa": "12043514-0 China Construction Bank",
+      "fecha creacion": "1/02/2021"
+    },
+    {
+      "rut": "11401919-4",
+      "nombre": "martin de olivera",
+      "correo": "martin.de.olivera@gmail.com",
+      "telefono": 986328728,
+      "rol": "Encargado",
+      "empresa": "12043514-0 China Construction Bank",
+      "fecha creacion": "3/09/2021"
+    },
+    {
+      "rut": "6361027-5",
+      "nombre": "natalio losada artigas",
+      "correo": "natalio.losada.artigas@gmail.com",
+      "telefono": 901470156,
+      "rol": "Gestor",
+      "empresa": "12039291-3 Saudi Aramco",
+      "fecha creacion": "18/05/2021"
+    },
+    {
+      "rut": "19451030-6",
+      "nombre": "olivia peñas guerrero",
+      "correo": "olivia.peñas.guerrero@hotmail.com",
+      "telefono": 986863993,
+      "rol": "Operador",
+      "empresa": "23690053-3 Berkshire Hathaway",
+      "fecha creacion": "11/02/2021"
+    },
+    {
+      "rut": "7991718-4",
+      "nombre": "plinio jove barbera",
+      "correo": "plinio.jove.barbera@yahoo.es",
+      "telefono": 914065787,
+      "rol": "Ejecutivo",
+      "empresa": "24566099-5 Industrial & Commercial Bank of China",
+      "fecha creacion": "17/06/2021"
+    },
+    {
+      "rut": "14349818-2",
+      "nombre": "ramiro mariño pozo",
+      "correo": "ramiro.mariño.pozo@gmail.com",
+      "telefono": 938317294,
+      "rol": "Encargado",
+      "empresa": "23690053-3 Berkshire Hathaway",
+      "fecha creacion": "31/07/2021"
+    },
+    {
+      "rut": "8668030-0",
+      "nombre": "rodrigo asenjo redondo",
+      "correo": "rodrigo.asenjo.redondo@yahoo.es",
+      "telefono": 995087591,
+      "rol": "Gestor",
+      "empresa": "12039291-3 Saudi Aramco",
+      "fecha creacion": "29/01/2021"
+    },
+    {
+      "rut": "8234214-1",
+      "nombre": "rogelio español caceres",
+      "correo": "rogelio.español.caceres@yahoo.es",
+      "telefono": 920158346,
+      "rol": "Gestor",
+      "empresa": "22724464-k Agricultural Bank of China",
+      "fecha creacion": "28/02/2021"
+    },
+    {
+      "rut": "11515373-0",
+      "nombre": "roque oliveras toledo",
+      "correo": "roque.oliveras.toledo@gmail.com",
+      "telefono": 900299120,
+      "rol": "Gestor",
+      "empresa": "12039291-3 Saudi Aramco",
+      "fecha creacion": "31/07/2021"
+    },
+    {
+      "rut": "9501888-2",
+      "nombre": "ruth jordan pacheco",
+      "correo": "ruth.jordan.pacheco@yahoo.es",
+      "telefono": 963589283,
+      "rol": "Gestor",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "31/07/2021"
+    },
+    {
+      "rut": "11546531-7",
+      "nombre": "salomon bernardo cobos",
+      "correo": "salomon.bernardo.cobos@hotmail.com",
+      "telefono": 940332347,
+      "rol": "Ejecutivo",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "11/04/2021"
+    },
+    {
+      "rut": "18863125-8",
+      "nombre": "samuel serna jurado",
+      "correo": "samuel.serna.jurado@hotmail.com",
+      "telefono": 975465988,
+      "rol": "Operador",
+      "empresa": "10882185-k SoftBank Group",
+      "fecha creacion": "6/08/2021"
+    },
+    {
+      "rut": "19545422-1",
+      "nombre": "severo murcia llorente",
+      "correo": "severo.murcia.llorente@yahoo.es",
+      "telefono": 904228262,
+      "rol": "Gestor",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "26/04/2021"
+    },
+    {
+      "rut": "13656607-5",
+      "nombre": "tomasa criado castell",
+      "correo": "tomasa.criado.castell@gmail.com",
+      "telefono": 972984726,
+      "rol": "Encargado",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "28/05/2021"
+    },
+    {
+      "rut": "7347506-6",
+      "nombre": "viviana donaire busquets",
+      "correo": "viviana.donaire.busquets@gmail.com",
+      "telefono": 955799228,
+      "rol": "Encargado",
+      "empresa": "6409492-0 Facebook",
+      "fecha creacion": "4/09/2021"
+    },
+    {
+      "rut": "11901999-0",
+      "nombre": "xiomara de coello",
+      "correo": "xiomara.de.coello@gmail.com",
+      "telefono": 969996442,
+      "rol": "Operador",
+      "empresa": "6737982-9 Apple",
+      "fecha creacion": "4/01/2021"
+    }
+  ];
+
+const monthsNames = {'01': 'Enero', '02': 'Febrero', '03': 'Marzo', '04': 'Abril', '05': 'Mayo',
+ '06': 'Junio', '07': 'Julio', '08': 'Agosto', '09': 'Septiembre', '10': 'Octubre', '11': 'Noviembre', 
+ '12': 'Diciembre'};
+
+// Function to write the name in the right format (iniciales en mayúscula)
+function formatName(name) {
+    let nameSplitted = name.split(' ');
+    for (let i = 0; i < nameSplitted.length; i++) {
+        nameSplitted[i] = nameSplitted[i][0].toUpperCase() + nameSplitted[i].substring(1);     
+    }
+    return nameSplitted.join(' '); 
+ }
+
+function load_users() {
+    // Sort data by date (oldest to newest)
+    data.sort(function(a,b) {
+        a = a["fecha creacion"].split('/').reverse().join('');
+        b = b["fecha creacion"].split('/').reverse().join('');
+        return a < b ? 1 : a > b ? -1 : 0;
+      }); 
+    // Sort data by role (alphabetic)
+    data.sort((a, b) => a.rol > b.rol ? 1 : -1);
+    
+    // Count by role and month
+    value_counts = {};
+    const months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    const roles = new Set(data.map((user) => user.rol));
+    for(let month of months){
+        value_counts[month] = {};
+        for(let role of roles){
+            let count = data.reduce((acc, cur) => cur.rol === role && 
+            cur["fecha creacion"].split('/')[1] === month ? ++acc : acc, 0);
+            value_counts[month][role] = count;
+        }
+    }
+
+    // Add data to review table
+    $('#reviewTable').append('<tr id="reviewHeader"><th>Mes</th>')
+    for(let role of roles){ 
+        $('#reviewHeader').append(`${`<th>Total ${role}</th>`}`)
+    }
+    $('#reviewHeader').append('</tr>');
+    
+    for(let month of months){
+        $('#reviewTable').append(`${`<tr id="reviewMonth${month}"><td>${monthsNames[month]}</td>`}`);
+        for(let role of roles){
+            $(`#reviewMonth${month}`).append(`${`<td>${value_counts[month][role]}</td>`}`)
+        }
+        $(`#reviewMonth${month}`).append('</tr>');
+    }
+
+    // Add users to the table
+    for(let i = 0; i < data.length; i++){
+        let nameFormatted = formatName(data[i].nombre);
+		$('#usersTable').append(`${`<tr><td>${data[i].rut}</td><td>${nameFormatted}</td>
+        <td>${data[i].correo}</td><td>${data[i].telefono}</td><td>${data[i].rol}</td>
+        <td>${data[i].empresa}</td><td>${data[i]["fecha creacion"]}
+        <button class="btn-trash"><i class="fa-solid fa-trash"></i></button></form></td></tr>`}`)
+	}
+}
+
+
+document.onload = load_users();
